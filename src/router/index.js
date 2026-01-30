d03a8cea9a7db6b2ed1c6eaf268f702ee83d2d95
paulfontes <pgfon22@gmail.com>
2026-01-29 23:26:48 -0700

    can login now with Auth0

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+const routes = [{ path: '/', name: 'home', component: HomeView }]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-
-const routes = [{ path: '/', name: 'home', component: HomeView }]
 
 export default router
