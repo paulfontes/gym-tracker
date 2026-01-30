aa7546bda0d885fbf457f1f9f43fec428588806f
paulfontes <pgfon22@gmail.com>
2026-01-29 23:07:46 -0700

    getting login to work

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import { createAuth0 } from '@auth0/auth0-vue'
+import router from './router'
 
 import App from './App.vue'
-import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+    },
+    cacheLocation: 'localstorage',
+    useRefreshTokens: true,
+  }),
+)
 app.use(router)
 
 app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue'
+const { loginWithRedirect, isAuthenticated, logout } = useAuth0()
+</script>
+
+<template>
+  <div class="container py-5">
+    <h1 class="mb-3">Gym Tracker</h1>
+
+    <button v-if="!isAuthenticated" class="btn btn-primary" @click="loginWithRedirect()">
+      Log in
+    </button>
+
+    <button
+      v-else
+      class="btn btn-outline-danger"
+      @click="logout({ logoutParams: { returnTo: window.location.origin } })"
+    >
+      Log out
+    </button>
+  </div>
+</template>
+<style></style>
